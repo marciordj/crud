4274227d1572d55323508a398da601c587aa9b8b
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2023-05-12 19:54:39 -0300

    update

--- core/index.ts
+++ core/index.ts
@@ -1,16 +1,47 @@
 import fs from 'fs'
 const DB_FILE_PATH = './core/db'
 
-function create(contet: string) {
-  fs.writeFileSync(DB_FILE_PATH, contet)
-  return contet
+interface Todo {
+  date: string;
+  content: string;
+  done: boolean;
 }
 
-function read() {
-  const readFile = fs.readFileSync(DB_FILE_PATH)
+function create(content: string) {
+  const todo: Todo = {
+    date: new Date().toISOString(),
+    content: content,
+    done: false
+  }
 
-  console.log(readFile)
+  const todos: Array<Todo> = [
+    ...read(),
+    todo
+  ]
+
+  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+    todos
+  }, null, 2))
+  return content
 }
 
-create('Testeeee')
-// read()
+function read(): Array<Todo> {
+  const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8')
+  const db = JSON.parse(dbString || "{}")
+
+  if (!db.todos) {
+    return []
+  }
+
+  return db.todos
+}
+
+function clearDb() {
+  fs.writeFileSync(DB_FILE_PATH, '')
+}
+
+
+clearDb()
+create('Aqui tem mais 1 teste')
+create('aqui tem dsjadjasj')
+console.log(read())
