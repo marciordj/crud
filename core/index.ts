de42a5200435dc0fb6bf36810dfb433a202d01d2
Marcio Rodrigues <luiz.rodrigues@gmail.com>
2023-05-20 14:55:45 -0300

    update

--- core/index.ts
+++ core/index.ts
@@ -1,47 +1,93 @@
-import fs from 'fs'
-const DB_FILE_PATH = './core/db'
+import fs from "fs";
+import { v4 as uuid } from "uuid";
+
+const DB_FILE_PATH = "./core/db";
 
 interface Todo {
+  id: string;
   date: string;
   content: string;
   done: boolean;
 }
 
-function create(content: string) {
+function create(content: string): Todo {
   const todo: Todo = {
+    id: uuid(),
     date: new Date().toISOString(),
     content: content,
-    done: false
-  }
+    done: false,
+  };
 
-  const todos: Array<Todo> = [
-    ...read(),
-    todo
-  ]
+  const todos: Array<Todo> = [...read(), todo];
 
-  fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
-    todos
-  }, null, 2))
-  return content
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        todos,
+      },
+      null,
+      2
+    )
+  );
+
+  return todo;
 }
 
 function read(): Array<Todo> {
-  const dbString = fs.readFileSync(DB_FILE_PATH, 'utf-8')
-  const db = JSON.parse(dbString || "{}")
+  const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+  const db = JSON.parse(dbString || "{}");
 
   if (!db.todos) {
-    return []
+    return [];
   }
 
-  return db.todos
+  return db.todos;
 }
 
 function clearDb() {
-  fs.writeFileSync(DB_FILE_PATH, '')
+  fs.writeFileSync(DB_FILE_PATH, "");
 }
 
+function update(id: string, partialTodo: Partial<Todo>): Todo {
+  let updatedTodo;
+  const readTodos = read();
 
-clearDb()
-create('Aqui tem mais 1 teste')
-create('aqui tem dsjadjasj')
-console.log(read())
+  readTodos.forEach((currentTodo) => {
+    const findTodoItem = currentTodo.id === id;
+
+    if (findTodoItem) {
+      updatedTodo = Object.assign(currentTodo, partialTodo);
+    }
+  });
+
+  fs.writeFileSync(
+    DB_FILE_PATH,
+    JSON.stringify(
+      {
+        readTodos,
+      },
+      null,
+      2
+    )
+  );
+
+  console.log("update -->", updatedTodo);
+
+  if (!updatedTodo) {
+    throw new Error("Please, provide a valid ID");
+  }
+
+  return updatedTodo;
+}
+
+clearDb();
+create("Teste conteudo 1");
+create("Teste conteudo 2");
+const terceiraTodo = create("Teste conteudo 3");
+update(terceiraTodo.id, {
+  content: "Atualizada",
+  done: true,
+});
+
+console.log(read());
