baa0ebe19e120d95e190488909e827fe00fc553c
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2023-05-23 22:46:30 -0300

    update

---
+++ pages/api/index.ts
@@ -0,0 +1,7 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(request: NextApiRequest, response: NextApiResponse) {
+  console.log(request.headers)
+  response.status(200).json({ message: 'hello world' })
+
+}
